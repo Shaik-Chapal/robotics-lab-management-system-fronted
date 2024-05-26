38fc9a08268d225103b386aaae43d7f8c47008f1
Shaik-Chapal <70383236+Shaik-Chapal@users.noreply.github.com>
2024-05-26 09:39:19 +0600

    host added

--- src/Redux/actionItems.js
+++ src/Redux/actionItems.js
@@ -3,4 +3,4 @@ export const Email = "EMAIL";
 export const Password = "PASSWORD";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGOUT = "LOGOUT";
-export const BASE_URL = "https://localhost:7161";
+export const BASE_URL = "http://www.rlms.skygreenblue.xyz";
